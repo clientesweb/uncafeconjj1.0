7afd90940608b7a4d3f6d247865202181e06855a
clientesweb <alexisifran32@gmail.com>
2025-02-26 22:38:15 -0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "un-cafe-con-jj-v1"
+
+const STATIC_ASSETS = ["/", "/manifest.json", "/icon-192x192.png", "/icon-512x512.png", "/favicon.ico"]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS)
+    }),
+  )
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name)))
+    }),
+  )
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    }),
+  )
+})
+
