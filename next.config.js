c0a3a5d83cb40a3ce379725a690f66d5c6d144ff
clientesweb <alexisifran32@gmail.com>
2025-02-27 00:20:56 -0300

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  reactStrictMode: true,
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-      },
-    ],
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
